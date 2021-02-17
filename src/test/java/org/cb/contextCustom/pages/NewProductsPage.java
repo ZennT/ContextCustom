f440efac2f3b4ac243deea64e3bea8cd6c28fa7f
ZennT <zenn.diamond@gmail.com>
2021-02-16 17:27:19 -0800

    NewProductsPage.java and NewProductsSteps.java files are updated

--- src/test/java/org/cb/contextCustom/pages/NewProductsPage.java
+++ src/test/java/org/cb/contextCustom/pages/NewProductsPage.java
@@ -1,4 +1,24 @@
 package org.cb.contextCustom.pages;
 
-public class NewProductsPage {
+import org.cb.contextCustom.stepdef.Base;
+import org.cb.contextCustom.utils.MyDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NewProductsPage extends Base {
+
+    public NewProductsPage() {
+        PageFactory.initElements(MyDriver.get(), this);
+    }
+
+    @FindBy(xpath = "//img[@alt='Embroidered Apron']")
+    public WebElement embroideredApronImage;
+
+    public void verifyEmbroideredApronImage(){
+        embroideredApronImage.isDisplayed();
+
+    }
 }
--- src/test/java/org/cb/contextCustom/stepdef/NewProductsSteps.java
+++ src/test/java/org/cb/contextCustom/stepdef/NewProductsSteps.java
@@ -2,13 +2,26 @@ package org.cb.contextCustom.stepdef;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.cb.contextCustom.pages.NewProductsPage;
+import org.cb.contextCustom.utils.ConfigurationReader;
+import org.cb.contextCustom.utils.MyDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NewProductsSteps {
+    Logger logger = LoggerFactory.getLogger(NewProductsSteps.class);
+    NewProductsPage newProductsPage = new NewProductsPage();
+
     @Given("User is on landing page")
     public void userIsOnLandingPage() {
+        MyDriver.get().get(ConfigurationReader.getProperty("baseUrl"));
+        logger.info("User is on LandingPage");
     }
 
     @Then("Verifies that Embroidered Apron picture is present")
     public void verifiesThatEmbroideredApronPictureIsPresent() {
+        newProductsPage.verifyEmbroideredApronImage();
+        logger.info("Apron image is present");
+
     }
 }
