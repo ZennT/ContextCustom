cd25d84fc716ff5b42c17a96c77cb7b1e1433ac7
ZennT <zenn.diamond@gmail.com>
2021-02-16 16:53:24 -0800

    NewProductsSteps.java file is created

---
+++ src/test/java/org/cb/contextCustom/stepdef/NewProductsSteps.java
@@ -0,0 +1,14 @@
+package org.cb.contextCustom.stepdef;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class NewProductsSteps {
+    @Given("User is on landing page")
+    public void userIsOnLandingPage() {
+    }
+
+    @Then("Verifies that Embroidered Apron picture is present")
+    public void verifiesThatEmbroideredApronPictureIsPresent() {
+    }
+}
---
+++ src/test/resources/features/NewProducts.feature
@@ -0,0 +1,4 @@
+Feature: Verifying new products
+  Scenario: New Products Verification
+    Given User is on landing page
+    Then Verifies that Embroidered Apron picture is present
